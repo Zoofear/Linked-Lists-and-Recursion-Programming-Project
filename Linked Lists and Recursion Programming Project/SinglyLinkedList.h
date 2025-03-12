48ccc1377b1ef479f008e8e782102d925847d37b
Zoofear <misterz0312@gmail.com>
2025-03-11 23:32:08 -0400

    Made the singly linked list with a tail, and counter. Was having issues figuring out how to set the tail pointer, and having an issue when deleting the tail where id get a memory exception. I fixed that by setting the pointer of the tail to point towards nullptr.

--- Linked Lists and Recursion Programming Project/SinglyLinkedList.h
+++ Linked Lists and Recursion Programming Project/SinglyLinkedList.h
@@ -14,19 +14,30 @@ struct Node {
 class SinglyLinkedList {
 private:
 	Node* head; // Pointer to the first node
+	Node* tail; // Pointer to the final node
+	int count = 0; //counting the elements in the list
 public:
-	SinglyLinkedList() : head(nullptr) {} // Constructor initializes the head to nullptr
-		// Function to insert a node at the beginning
-		void insertAtBeginning(int value) {
+	SinglyLinkedList() : head(nullptr), tail(nullptr), count(0) {} // Constructor initializes the head to nullptr
+	
+	// Function to insert a node at the beginning
+	void insertAtBeginning(int value) {
 		Node* newNode = new Node(value);
 		newNode->next = head;
 		head = newNode;
+		count++;
+
+		if (tail == nullptr)
+		{
+			tail = newNode;
+			return;
+		}
 	}
 	// Function to insert a node at the end
 	void insertAtEnd(int value) {
 		Node* newNode = new Node(value);
 		if (!head) {
 			head = newNode;
+			count++;
 			return;
 		}
 		Node* temp = head;
@@ -34,6 +45,10 @@ public:
 			temp = temp->next;
 		}
 		temp->next = newNode;
+		tail = newNode;
+
+		count++;
+
 	}
 	// Function to delete the first occurrence of a value
 	void deleteValue(int value) {
@@ -42,7 +57,29 @@ public:
 			Node* toDelete = head;
 			head = head->next;
 			delete toDelete;
+			count--;
 			return;
+		}
+		if (tail->data == value)
+		{
+			Node* temp = head;
+			while (temp->next && temp->next->data != value)
+			{
+				temp = temp->next;
+			}
+
+			tail = temp;
+			
+			Node* toDelete = temp->next;
+
+			delete toDelete;
+			
+			temp->next = nullptr;
+
+			count--;
+			
+			return;
+
 		}
 		Node* temp = head;
 		while (temp->next && temp->next->data != value) {
@@ -52,10 +89,14 @@ public:
 			Node* toDelete = temp->next;
 			temp->next = temp->next->next;
 			delete toDelete;
+			count--;
 		}
 	}
 	// Function to display the linked list
 	void display() {
+
+		std::cout << tail->data << " count:" << count << std::endl;
+
 		Node* temp = head;
 		while (temp) {
 			std::cout << temp->data << " -> ";
@@ -71,5 +112,6 @@ public:
 			head = head->next;
 			delete temp;
 		}
+		count = 0;
 	}
 };
