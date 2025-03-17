07f3ec3b0656fa7b15292c0630a9eb239ac5120b
Zoofear <misterz0312@gmail.com>
2025-03-17 11:34:36 -0400

    Added the even odd sorting functionality to singly linked list

--- Linked Lists and Recursion Programming Project/SinglyLinkedList.h
+++ Linked Lists and Recursion Programming Project/SinglyLinkedList.h
@@ -92,10 +92,58 @@ public:
 			count--;
 		}
 	}
+
+	void evenOdd(SinglyLinkedList &even, SinglyLinkedList &odd)
+	{
+		if (!head)	//check to make sure that your normal list is filled
+		{
+			std::cout << "You need to fill your base list first" << std::endl;
+			return;
+		}
+		Node* temp = head;
+		while (temp)
+		{
+			double num = static_cast<double>(temp->data);		//Need to set a temporary variable to the double to check if it is cleanly divisible by two
+			if (temp->data / 2 == num / 2)						//Even odd checker
+			{
+				if (even.count == 0)							//Checks whether or not list is empty
+				{
+					even.head = temp;							//sets the head pointer in the list even to the current pointer in this list
+				}
+				else
+				{
+					even.tail->next = temp;						//sets the next link of the tail pointer in the list even to the current pointer in this list
+				}
+				even.tail = temp;								//sets the even list tail to the current pointer
+				even.count++;									//adds one to the current count in the even list
+				temp = temp->next;								//iterates to the next node in the list
+				even.tail->next = nullptr;						//sets up the next node in the even list
+			}
+			else if (temp->data / 2 != num / 2)
+			{
+				if (odd.count == 0)
+				{
+					odd.head = temp;
+				}
+				else
+				{
+					odd.tail->next = temp;
+				}
+				odd.tail = temp;
+				odd.count++;
+				temp = temp->next;
+				odd.tail->next = nullptr;
+			}
+		}
+		head = nullptr;
+		tail = nullptr;
+		count = 0;
+	}
+
 	// Function to display the linked list
 	void display() {
 
-		std::cout << tail->data << " count:" << count << std::endl;
+		std::cout << " Count (" << count << ") List :";
 
 		Node* temp = head;
 		while (temp) {
--- Linked Lists and Recursion Programming Project/project 1.cpp
+++ Linked Lists and Recursion Programming Project/project 1.cpp
@@ -11,64 +11,33 @@ int main()
 
 	SinglyLinkedList list1;
 
-	list1.insertAtBeginning(50);
-	list1.insertAtBeginning(40);
-	list1.insertAtBeginning(30);
-	list1.insertAtBeginning(20);
-	list1.insertAtBeginning(10);
-
-	cout << "displaying list1" << endl;
-	
-	list1.display();
-
-	list1.deleteValue(10);
-
-	cout << "deleting the first value 10" << endl;
-
-	list1.display();
-
-	list1.deleteValue(50);
-
-	cout << "deleting the last value 50" << endl;
-
-	list1.display();
-
-	cout << "deleting the middle value 30" << endl;
-
-	list1.deleteValue(30);
-
-	list1.display();
-	
-	list1.~SinglyLinkedList();
-
-	list1.insertAtEnd(50);
-	list1.insertAtEnd(40);
-	list1.insertAtEnd(30);
-	list1.insertAtEnd(20);
+	list1.insertAtEnd(34);
+	list1.insertAtEnd(21);
+	list1.insertAtEnd(10);
+	list1.insertAtEnd(15);
+	list1.insertAtEnd(90);
+	list1.insertAtEnd(66);
+	list1.insertAtEnd(53);
+	list1.insertAtEnd(7);
+	list1.insertAtEnd(88);
+	list1.insertAtEnd(36);
+	list1.insertAtEnd(90);
+	list1.insertAtEnd(11);
+	list1.insertAtEnd(17);
+	list1.insertAtEnd(24);
 	list1.insertAtEnd(10);
 
-	cout << "displaying list2" << endl;
-
 	list1.display();
 
-	cout << "deleting the first value 50" << endl;
-	
-	list1.deleteValue(50);
+	SinglyLinkedList even;
+
+	SinglyLinkedList odd;
+
+	list1.evenOdd(even, odd);
+
+	even.display();
+
+	odd.display();
 
 	list1.display();
-
-	cout << "deleting the last value 10" << endl;
-
-	list1.deleteValue(10);
-
-	list1.display();
-
-	list1.deleteValue(30);
-
-	cout << "deleting the middle value 30" << endl;
-
-	list1.display();
-
-	list1.~SinglyLinkedList();
-
 };
